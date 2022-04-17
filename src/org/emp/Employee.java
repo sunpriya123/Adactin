b5f285a0cc74a8bf2abcac9f9d0cc72437f83eb1
sunpriya123 <sunpriyathamarai@gmail.com>
2022-04-17 21:48:07 +0530

    Extent report Generated

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,44 @@
+package org.emp;
+
+public class Employee {
+	private void empID() {
+		// TODO Auto-generated method stub
+		System.out.println("emp code=17536");
+
+	}
+	private void empName() {
+		// TODO Auto-generated method stub
+		System.out.println("name=sun priya");
+
+	}
+	private void empDob() {
+		// TODO Auto-generated method stub
+		System.out.println("dob=24/02/1997");
+
+	}
+	private void empPhone() {
+		// TODO Auto-generated method stub
+		System.out.println("mob num=7339044396");
+
+	}
+	private void empEmail() {
+		// TODO Auto-generated method stub
+		System.out.println("email=sunpriyathamarai@gmail.com");
+
+	}
+	private void empAddress() {
+		// TODO Auto-generated method stub
+		System.out.println("address=trichy");
+
+	}
+	public static void main(String[] args) {
+		Employee obj=new Employee();
+		obj.empID();
+		obj.empName();
+		obj.empDob();
+		obj.empPhone();
+		obj.empEmail();
+		obj.empAddress();
+	}
+
+}
